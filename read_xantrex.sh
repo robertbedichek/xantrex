e5ef686c7a6dd05c492ab7353a1f1ef80d71f317
Robert Bedichek <robert@bedichek.org>
2025-09-19 21:41:54 -0700

    1. Add xantrex.html, which has the JS code to generate graphs of Xantrex XW currents and voltages and the JS code to generate kilowatt-vs-time of the two Enhphase systems. 2. Fixed bugs in read_xantrex.sh and now that script generates the data lines that the xantrex.html JS code expects.

--- read_xantrex.sh
+++ read_xantrex.sh
@@ -4,16 +4,24 @@ DATA_DIR=/var/log/xantrex
 Xantrex_name=xantrex
 Xantrex_port=80
 SOC_FILE=${DATA_DIR}/soc.txt
+CURL=/usr/bin/curl
+XANTREX_DIR=/Users/robertbedichek/Documents/Arduino/xantrex
+TOKEN=`cat ${XANTREX_DIR}/token.txt`
 
 # Two 48V strings of DC6-400.  Watt hours = 48 * 2 * 400
 BATTERY_WATT_HOURS=40000
 
-/Users/robertbedichek/Documents/Arduino/xantrex/sample_xantrex.py >>${DATA_DIR}/xantrex_inverter.xml.temp 2> /dev/null
+ if [[ "$(/bin/date +%M)" = 00 ]]; then
+   echo "# Date     Time     DCV  DCI AC-In-Amps AC-In-Volts  AC-Out-Amps AC-Out-Volts SoC Enphase-kW" | /usr/bin/env ssh -i $HOME/.ssh/id_rsa root@bedichek.org "cat >> /var/www/html/home/xantrex.txt"
+ fi
+
+/bin/rm -f ${DATA_DIR}/xantrex_inverter.xml.temp ${DATA_DIR}/xantrex_inverter.xml
+${XANTREX_DIR}/sample_xantrex.py >>${DATA_DIR}/xantrex_inverter.xml.temp 2> /dev/null
 
 if [ -s "${DATA_DIR}/xantrex_inverter.xml.temp" ]; then
   mv ${DATA_DIR}/xantrex_inverter.xml.temp ${DATA_DIR}/xantrex_inverter.xml
 
-  echo -n `date +"%b %d %H:%M:%S %Y"` >${DATA_DIR}/xantrex.data
+  echo -n `date +"%Y-%m-%d %H:%M:%S"` >${DATA_DIR}/xantrex.data
 
 # First, the inverter's DC input voltage and current
 
@@ -28,18 +36,25 @@ if [ -s "${DATA_DIR}/xantrex_inverter.xml.temp" ]; then
   fi
   SOC=`cat ${SOC_FILE}`
   NEW_SOC=${SOC}
-  if [[ ${DCV} -gt 53 ]]; then
-    NEW_SOC=${BATTER_WATT_HOURS}
-  else
-    if [[ ${DCI} -lt 0 ]]; then
-      # Number of watt-hours used in the previous two minute interval
-      WATT_HOURS=$(echo "scale=2; $DCV * $DCI / 30.0" | bc)
 
-      NEW_SOC=$(echo "scale=2; $SOC + $WATT_HOURS" | bc)
-      echo ${NEW_SOC} > ${SOC_FILE}
+  fgt() { echo "$1 > $2" | bc -l; }
+  flt() { echo "$1 < $2" | bc -l; }
+  
+  if (( $(fgt "$DCV" 53) )); then
+    # (verify this var name—did you mean BATTERY_WATT_HOURS?)
+    NEW_SOC="${BATTERY_WATT_HOURS}"
+  else
+    if (( $(flt "$DCI" 0) )); then
+      # Watt-hours over a 2-minute interval
+      WATT_HOURS=$(echo "$DCV * $DCI / 30" | bc -l)
+  
+      NEW_SOC=$(echo "$SOC + ($WATT_HOURS)" | bc -l)
+  
+      # write with 2 decimals
+      printf '%.2f\n' "$NEW_SOC" > "$SOC_FILE"
     fi
   fi
-
+  
 # The inverter's AC1 (Utility)
   cat ${DATA_DIR}/xantrex_inverter.xml          | egrep \"ACI\"    | sed -e 's/.*value="\([0-9\.-]*\)".*/ \1 /' |tr '\n' ' ' >>${DATA_DIR}/xantrex.data
   cat ${DATA_DIR}/xantrex_inverter.xml          | egrep \"ACV\"    | sed -e 's/.*value="\([0-9-]*\).*/ \1 /'    |tr '\n' ' ' >>${DATA_DIR}/xantrex.data
@@ -47,12 +62,50 @@ if [ -s "${DATA_DIR}/xantrex_inverter.xml.temp" ]; then
 # The inverter's Load (critical output)
   cat ${DATA_DIR}/xantrex_inverter.xml          | egrep \"ACOutI\" | sed -e 's/.*value="\([0-9\.-]*\)".*/ \1 /' |tr '\n' ' ' >>${DATA_DIR}/xantrex.data
   cat ${DATA_DIR}/xantrex_inverter.xml          | egrep \"ACOutV\" | sed -e 's/.*value="\([0-9\.-]*\)".*/ \1 /' |tr '\n' ' ' >>${DATA_DIR}/xantrex.data
-  echo $(echo "scale=2; $NEW_SOC * 100.0 / $BATTERY_WATT_HOURS" | bc)  >>${DATA_DIR}/xantrex.data
 
+  echo -n $(echo "scale=2; $NEW_SOC * 100.0 / $BATTERY_WATT_HOURS" | bc)  >>${DATA_DIR}/xantrex.data
 
- if [[ "$(/bin/date +%M)" = 0 ]]; then
-   echo "# Date     Time     DCV  DCI AC-In-Amps AC-In-Volts  AC-Out-Amps AC-Out-Volts" | /usr/bin/env ssh -i $HOME/.ssh/id_rsa root@bedichek.org "cat >> /var/www/html/home/xantrex.txt"
- fi
- cat ${DATA_DIR}/xantrex.data | /usr/bin/env ssh -i $HOME/.ssh/id_rsa root@bedichek.org "cat >> /var/www/html/home/xantrex.txt"
+  # This script is run every two minutes by a cron job.  Here we poll the Enphase
+  # communications gateway, extract the information, put
+  # it into a gnuplot-compatible format, and create a new plot.
+  
+  Enphase_name="enphase"
+  Enphase_port="80"
+  
+  /bin/rm -f ${DATA_DIR}/old_enphase.temp ${DATA_DIR}/old_enphase.http
+
+  ${CURL} -s http://${Enphase_name}:${Enphase_port}/home >${DATA_DIR}/old_enphase.temp 2> /dev/null
+  
+  if [ -s "${DATA_DIR}/old_enphase.temp" ]; then
+    mv ${DATA_DIR}/old_enphase.temp ${DATA_DIR}/old_enphase.http
+    egrep 'Currently generating'  ${DATA_DIR}/old_enphase.http  >${DATA_DIR}/old_enphase.line
+  
+  # $DATA_DIR/old_enphase.line is a one-line file like this:
+  #
+  #            <tr><td>Lifetime generation</td>    <td> 27.8 kWh</td></tr><tr><td>Currently generating</td>    <td>    0 W</td></tr>
+  #
+  # The "currently generating" field can be Watts or kilo-Watts.  We have to determine which
+  
+    egrep ' W</td>' ${DATA_DIR}/old_enphase.line >/dev/null
+  
+    if [ "$?" == "0" ]; then
+      OLD_ENPHASE_KW=`cat ${DATA_DIR}/old_enphase.line | sed -e 's/.*generating<\/td> *<td> *\([0-9\.]*\) W.*/0.\1/'`
+    else
+      OLD_ENPHASE_KW=`cat ${DATA_DIR}/old_enphase.line | sed -e 's/.*generating<\/td> *<td> *\([0-9\.]*\) kW.*/\1/'`
+    fi
+  
+    echo -n " ${OLD_ENPHASE_KW}" >>${DATA_DIR}/xantrex.data
+  fi
+  ${CURL} -fsSk -H "Accept: application/json" -H "Authorization: Bearer $TOKEN" "https://192.168.87.32/api/v1/production" | jq . >${DATA_DIR}/new_enphase.json
+  if [ -s "${DATA_DIR}/new_enphase.json" ]; then
+    NEW_ENPHASE_W=`jq -r '.wattsNow // 0' ${DATA_DIR}/new_enphase.json`
+
+    NEW_ENPHASE_KW=$(echo "scale=2; $NEW_ENPHASE_W / 1000.0" | bc)
+    echo -n " ${NEW_ENPHASE_KW}" >>${DATA_DIR}/xantrex.data
+  fi
+
+  echo "" >>${DATA_DIR}/xantrex.data
+
+  cat ${DATA_DIR}/xantrex.data | /usr/bin/env ssh -i $HOME/.ssh/id_rsa root@bedichek.org "cat >> /var/www/html/home/xantrex.txt"
 
 fi
